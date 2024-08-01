33c9d7db68417d0c6ba5d2539827024e41f8ae45
dev_hoa <nguyentangthaihoa21011999@gmail.com>
2024-08-01 16:31:05 +0700

    update readme & run.sh

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Scraping-Project
-Scrap data from website
+Scrap data from website https://www.glamira.com/
+
+## Description
+Project scrap all data on webpage Glamira category
+ * Ring
+ * Necklace
+ * Earring
+ * Apple Watch Case 
+
+
+
+
+
+
+## How to use
+
+1. Git clone project
+2. chmod +x run.sh ( allow file execute permisson )
+3. run ./run.sh 
+    * Create .venv 
+    * Install neccessary module from requirement.txt
+    * exec glamira/glamira/spiders/production/glamira_dev.py
+
+4. Data and image will be located at glamira/glamira/image_folder and glamira/glamira/data
+
+    
+
+Using CrawlerProcess to run multi spiders in one process
+
+Total runtime: 10hrs for 9Gbs image + json line file
---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if a Python interpreter is provided, otherwise default to 'python3'
+PYTHON=${1:-python3}
+
+# Name of the virtual environment directory
+VENV_DIR="venv"
+
+# Create a virtual environment
+$PYTHON -m venv $VENV_DIR
+
+# Check if the virtual environment was created successfully
+if [ ! -d "$VENV_DIR" ]; then
+  echo "Failed to create virtual environment"
+  exit 1
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+# Check if a requirements file is provided
+if [ -f "requirements.txt" ]; then
+  # Install the dependencies
+  pip install -r requirements.txt
+else
+  echo "requirements.txt not found. No dependencies installed."
+fi
+
+# Confirm the setup
+echo "Virtual environment setup complete. To activate it, run:"
+echo "source $VENV_DIR/bin/activate"
+echo "To run the project"
+echo "step 1: cd glamira/glamira "
+echo "step 2: python3 spiders/production/glamira_dev.py"
+echo "the data jsonl will be located at glamira/glamira/data"
+echo "the image will be located at glamira/glamira/image"
